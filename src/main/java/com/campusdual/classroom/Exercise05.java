4da246fe4ec2f897bccd0aab0acbfd57f10f7415
Jesus Blanco Miguez <119857344+eluiui@users.noreply.github.com>
2024-09-24 22:28:46 +0200

    metodo lessorequal

--- src/main/java/com/campusdual/classroom/Exercise05.java
+++ src/main/java/com/campusdual/classroom/Exercise05.java
@@ -9,13 +9,18 @@ public class Exercise05 {
     // Si el valor es menor, tiene que devolver el siguiente mensaje: X es menor que Y
     // Si el valor es mayor o igual, tiene que devolver el siguiente mensaje: X es mayor o igual que Y
     public static void checkLessOrGreaterEqual(int n1, int n2) {
-
+        if(n1>n2){
+            System.out.println("X es menor que Y");
+        }else {
+            System.out.println("X es mayor o igual que Y");
+        }
     }
 
     //TODO ↓
     // Si es múltiplo, tiene que devolver el siguiente mensaje: X es múltiplo de Y
     // Si no es múltiplo, tiene que devolver el siguiente mensaje: X no es múltiplo de Y
     public static void checkMultiply(int n1, int n2) {
+
 
     }
 
